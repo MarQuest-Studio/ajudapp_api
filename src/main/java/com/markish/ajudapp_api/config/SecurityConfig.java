ed4bdc7a2cbba3dbdf99af552eb6645d33c21475
JonhIsh <rodriguezjonhy04@gmail.com>
2026-01-22 19:51:28 +0000

    Fixing sonar security issue

--- src/main/java/com/markish/ajudapp_api/config/SecurityConfig.java
+++ src/main/java/com/markish/ajudapp_api/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,7 +19,6 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/api/public/**").permitAll()
